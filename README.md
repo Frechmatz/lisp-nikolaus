396757cf19c2d609b63374321d84e16d1ab827f7
Oliver Voeltz <ovoeltz@gmx.de>
2015-07-01 21:31:11 +0200

    Pretty printing and elimination of global variable

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # lisp-nikolaus
-Solution of 'Haus vom Nikolaus' implemented in Lisp
+#### Solution of 'Haus vom Nikolaus' implemented in Lisp
 
-(lets-go) => Calculates all solutions
+### Installation
 
-An interactive mode where a start node can be chosen and pretty-printing of pathes is in work.
+Download nikolaus.lisp
+
+### Running the program
+
+To run the program you need an implementation of Common Lisp such as clisp (http://www.clisp.org).
+Enter into the directory where you've downloaded the source file and start the Lisp REPL. 
+On the REPL command prompt enter
+
+```bash
+(load "nikolaus.lisp")
+(lets-go)
+```
+
+All solutions will be printed out.
 
--- nikolaus.lisp
+++ nikolaus.lisp
@@ -31,9 +31,6 @@ for A => B => C:
     (E (C B D))
     ))
 
-(defparameter *solutions* '())
-
-
 #|
 check if (A B) has been traversed
 |#
@@ -75,14 +72,16 @@ any given path => solution
 ((A A)) => ...
 ((E D) (D C) (C C)) => ...
 |#
-(defun iterate-impl (path) 
-  (if (equal (length path) 9)
-      (push path *solutions*)
-    (loop for node in (get-edges path) do
-	  (if (not (is-bidirectional-traversal-p (list node (last-visited-node path)) path))
-		   (iterate-impl (add-step path node))
-	  ))))
-
+(defun iterate-impl (path)
+  (let ((solutions ()))
+    (labels ((inner (path)
+	    (if (equal (length path) 9)
+		(push path solutions)
+	      (loop for node in (get-edges path) do
+		    (if (not (is-bidirectional-traversal-p (list node (last-visited-node path)) path))
+			(inner (add-step path node)))))))
+	    (inner path))
+    solutions))
 
 #|
 (A B E) => all solutions of given nodes
@@ -90,20 +89,53 @@ alternative implementation:
 (mapcar #'(lambda (node) (iterate-impl (list (list node node)))) nodes)
 |#
 (defun nikolaus (nodes)
-  (defparameter *solutions* ())
-  (loop for i in nodes do
-	(iterate-impl (list (list i i)))
+  (let ((node-solutions ()))
+    (loop for i in nodes do
+	(push (iterate-impl (list (list i i))) node-solutions)
 	)
+    node-solutions))
 
+;;
+;; path: solution in reverse order
+;; start node X of path is represented by (X X)
+;; tupels are (<target-node> <start-node>)
+;; example for start node D: ((E D) (D C) (C E) (E B) (B C) (C A) (A B) (B D) (D D))
+(defun format-solution-path (path)
+  ;; reverse and skip start tupel (X X)
+  (let ((first t))
+    (dolist (step (cdr (reverse path)))
+      (if first
+	  (format t "~a -> ~a" (second step) (first step))
+	(format t " -> ~a" (first step))
+	)
+      (setf first nil)
+      )))
+
+(defun format-house ()
+  (format t "~cA" #\tab)
+  (format t "~%B~c~cC" #\tab #\tab)
+  (format t "~%D~c~cE~%" #\tab #\tab)
   )
-
+  
+;;
+;; Entry point
+;;
 (defun lets-go ()
-  (nikolaus '(A B C D E))
-  (print "Solutions:")
-  (print (mapcar #'(lambda (path) (reverse (mapcar #'car path))) *solutions*))
-  (print (list "Number of solutions found:" (length *solutions*)))
- )
-
-(lets-go)
+  (format t "~%Welcome to 'Haus vom Nikolaus'~%")
+  (format-house)
+  (let ((node-solutions (nikolaus '(A B C D E))))
+    ;; solutions: for each node: a list of solutions or nil
+    (let ((flattened-solutions ()))
+      (dolist (node node-solutions)
+	(if node (dolist (s node)
+		   (push s flattened-solutions)))
+	)
+      (format t "Solutions:~%")
+      (dolist (solution flattened-solutions)
+	(format-solution-path solution)
+	(princ #\newline)
+	)
+      (format t "Number of solutions found: ~a~%" (length flattened-solutions))
+      )))
 
 
